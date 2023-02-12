bfcf17951c40d9252f3512d3c09676b84db03cc2
n3II <118652388+skroud@users.noreply.github.com>
2023-02-12 12:05:02 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import sys
 import subprocess
 import json
 import time
-import datetime
 from concurrent.futures import ThreadPoolExecutor
 
 from assets.funcs.packet_per_sec import get_packets_per_second
