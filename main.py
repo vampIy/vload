50e1ec4a1ca2cbdb1264b11a6f05653fd13c1023
n3II <118652388+skroud@users.noreply.github.com>
2023-02-18 13:17:06 -0600

    h

--- main.py
+++ main.py
@@ -8,59 +8,32 @@ from colorama import Fore
 import subprocess
 from concurrent.futures import ThreadPoolExecutor
 
-with open("config.json", encoding="utf-8") as config_file:
-    config = json.load(config_file)
+interface = "eth0"
 
-    interface = config.get("interface")
-    ip = config.get("ip")
-    port = config.get("port")
-    type = config.get("type")
-
-def get_last_attacked_source_port():
-    with open('/var/log/syslog', 'r') as f:
-        log_lines = f.readlines()
-
-    ddos_regex = r'.*SYN_RECV.*SRCPT=(\d+).*'
-
-    for line in reversed(log_lines):
-        match = re.match(ddos_regex, line)
-        if match:
-            attacked_src_port = match.group(1)
-            return attacked_src_port
-    else:
-        return
-    
-def get_last_attacked_destination_port():
-    with open('/var/log/syslog', 'r') as f:
-        log_lines = f.readlines()
-
-    ddos_regex = r'.*SYN_RECV.*DPT=(\d+).*'
-
-    for line in reversed(log_lines):
-        match = re.match(ddos_regex, line)
-        if match:
-            attacked_dest_port = match.group(1)
-            return attacked_dest_port
-    else:
-        return
-
-def get_megabits_per_second(interface: str) -> int:
-    old_b = subprocess.check_output("grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $1}\'" % interface, shell=True)
-    old_b2 = int(float(old_b.decode('utf8').rstrip()))
+def get_incomingmb_per_second(interface: str) -> int:
+    net_io_counters = psutil.net_io_counters()
+    bytes_recv = net_io_counters.bytes_recv
     time.sleep(1)
-    new_b = subprocess.check_output("grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $1}\'" % interface, shell=True)
-    new_b2 = int(float(new_b.decode('utf8').rstrip()))
-    byte = (new_b2 - old_b2)
-    mbps = byte / 125000
-    return round(mbps)
+    net_io_counters = psutil.net_io_counters()
+    bytes_recv_diff = net_io_counters.bytes_recv - bytes_recv
+    inbound_mbps = bytes_recv_diff / 1e6 * 8
+    return inbound_mbps
+
+def get_outgoingmb_per_second(interface: str) -> int:
+    net_io_counters = psutil.net_io_counters()
+    bytes_sent = net_io_counters.bytes_sent
+    time.sleep(1)
+    net_io_counters = psutil.net_io_counters()
+    bytes_sent_diff = net_io_counters.bytes_sent - bytes_sent
+    outbound_mbps = bytes_sent_diff / 1e6 * 8
+    return outbound_mbps
 
 def get_packets_per_second(interface: str) -> int:
-    old_ps = subprocess.check_output("grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $2}\'" % interface, shell=True)
-    old_ps2 = int(float(old_ps.decode('utf8').rstrip()))
+    packets_1 = int(psutil.net_io_counters().packets_recv)
     time.sleep(1)
-    new_ps = subprocess.check_output('grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $2}\'' % interface, shell=True)
-    new_ps2 = int(float(new_ps.decode('utf8').rstrip()))
-    return round(new_ps2 - old_ps2)
+    packets_2 = int(psutil.net_io_counters().packets_recv)
+    pps = packets_2 - packets_1
+    return pps
 
 def get_cpu_percentage():
     cpu_percent = psutil.cpu_percent()
@@ -71,12 +44,10 @@ def get_ram_percentage():
     return ram_percent
 
 def get_server_status():
-    old_ps = subprocess.check_output("grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $2}\'" % interface, shell=True)
-    old_ps2 = int(float(old_ps.decode('utf8').rstrip()))
+    packets_1 = int(psutil.net_io_counters().packets_recv)
     time.sleep(1)
-    new_ps = subprocess.check_output('grep %s /proc/net/dev | cut -d : -f2 | awk \'{print $2}\'' % interface, shell=True)
-    new_ps2 = int(float(new_ps.decode('utf8').rstrip()))
-    pps = new_ps2 - old_ps2
+    packets_2 = int(psutil.net_io_counters().packets_recv)
+    pps = packets_2 - packets_1
 
     if int(pps) > 2500:
         load = "High"
@@ -85,7 +56,9 @@ def get_server_status():
     if int(pps) > 500 < 1000:
         load = "low"
     if int(pps) > 0 < 2500:
-        load = "essentially no load"
+        load = "None"
+
+    return load
 
 def main() -> None:
     sys.stdout.write("\033[?25l")
@@ -95,24 +68,27 @@ def main() -> None:
 
     with ThreadPoolExecutor() as executor:
         while True:
-            mbps_future = executor.submit(get_megabits_per_second, interface)
+            inboundmb_future = executor.submit(get_incomingmb_per_second, interface)
+            outboundmb_future = executor.submit(get_outgoingmb_per_second, interface)
             pps_future = executor.submit(get_packets_per_second, interface)
             get_ram_future = executor.submit(get_ram_percentage)
             get_cpu_future = executor.submit(get_cpu_percentage)
-            get_srcport_future = executor.submit(get_last_attacked_source_port)
-            get_dstport_future = executor.submit(get_last_attacked_destination_port)
             get_servstatus_future = executor.submit(get_server_status)
     
-            mb = mbps_future.result()
-            p = pps_future.result()
-            r = get_ram_future.result()
-            c = get_cpu_future.result()
-            s = get_srcport_future.result()
-            d = get_dstport_future.result()
-            ss = get_servstatus_future.result()
+            inmb = inboundmb_future.result()
+            outmb = outboundmb_future.result()
+            pps = pps_future.result()
+            ram = get_ram_future.result()
+            cpu = get_cpu_future.result()
+            stat = get_servstatus_future.result()
 
-            print(f"Server Status: {ss}  |  Mbits/s: {mb}  |  Packets/s: {p}  |  Cpu: {c}%  |  Ram: {r}%")
-            for i in range(1):
+            print(f"Server Status: {stat}")
+            print(f"Packets: {pps}")
+            print(f"Incoming: {inmb} Mbit/s")
+            print(f"Outgoing: {outmb} Mbit/s")
+            print(f"Cpu Usage: {cpu}%")
+            print(f"Ram Usage: {ram}%")
+            for i in range(5):
                 sys.stdout.write('\x1b[1A')
 
 if __name__ == '__main__':
